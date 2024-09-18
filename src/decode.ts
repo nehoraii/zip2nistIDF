bb2e57df800f86f150d07be1e5508600d825f93f
doreen-schwartz <doreen.schwartz01@gmail.com>
2024-09-18 14:50:15 +0300

    fix nist import

--- src/decode.ts
+++ src/decode.ts
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs';
 
-import { nistDecode } from '../node-nist/src/index';
+import { nistDecode } from 'node-nist';
 
 // const buffer = fs.readFileSync('data/300056859.tdf');
 const buffer = fs.readFileSync('data/215270121T_001.tdf');
